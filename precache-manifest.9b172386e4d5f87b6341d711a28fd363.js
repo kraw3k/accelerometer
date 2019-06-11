48c65f38c1407f07bea37b50b1ce54d6a2a7f3b3
kraw3k <kacper5047@gmail.com>
2019-06-11 17:14:07 +0200

    Updates

---
+++ precache-manifest.9b172386e4d5f87b6341d711a28fd363.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbbd0e3c55ccc246aa50cf3e991108b",
+    "url": "/accelerometer-app/index.html"
+  },
+  {
+    "revision": "8b22ed2b7afff1868551",
+    "url": "/accelerometer-app/static/js/2.20e277fb.chunk.js"
+  },
+  {
+    "revision": "dce57c94eed5a94b1d47",
+    "url": "/accelerometer-app/static/js/main.e3622da2.chunk.js"
+  },
+  {
+    "revision": "84181f36a4a88fb565c0",
+    "url": "/accelerometer-app/static/js/runtime~main.94d36c1b.js"
+  }
+]);
